0a8925ecf21b51a0bb1ca63c89826d71eae9a0d6
edwardlthompson <edwardleethompson@gmail.com>
2026-03-14 07:55:35 -0400

    chore: bump version to 1.4.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 }
 
-val appVersion = "1.4.3"
+val appVersion = "1.4.4"
 
 android {
     namespace = "com.multiappshare"
@@ -14,7 +14,7 @@ android {
         applicationId = "com.multiappshare"
         minSdk = 26
         targetSdk = 36
-        versionCode = 9
+        versionCode = 10
         versionName = appVersion
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
